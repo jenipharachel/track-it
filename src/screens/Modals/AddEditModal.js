cd14703f06603e83c9746094ae19337769e203c1
Jenipha Rachel <jenipha.rachel@bigbinary.com>
2021-07-15 17:12:37 +0530

    datepicker

--- src/screens/Modals/AddEditModal.js
+++ src/screens/Modals/AddEditModal.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
   TouchableOpacity,
   StyleSheet,
   TextInput,
+  Button,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/AntDesign';
 // import {TextBox, SaveButton} from '../../components';
 import colors from '../../theme/colors';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import moment from 'moment';
 
 const AddEditModal = props => {
   const [amount, onChangeAmt] = React.useState('');
   const [desc, onChangeDesc] = React.useState('');
-  const [date, onChangeDate] = React.useState('');
+  let currentDate = moment(new Date()).format('LL');
+  const [date, setDate] = useState(currentDate);
+  const dateTime = new Date();
+  const [mode, setMode] = useState('date');
+  const [show, setShow] = useState(false);
 
   const TextBox = (func, value, placeholder) => {
     return (
@@ -24,6 +31,37 @@ const AddEditModal = props => {
         placeholder={placeholder}
       />
     );
+  };
+
+  const DateBox = (value, placeholder) => {
+    return (
+      <TouchableOpacity
+        style={{flex: 0.08, margin: 15}}
+        onPress={showDatepicker}>
+        <TextInput
+          style={[styles.input, {flex: 1, margin: 0}]}
+          value={`${value}`}
+          placeholder={placeholder}
+          editable={false}
+        />
+      </TouchableOpacity>
+    );
+  };
+
+  const onChange = (event, selectedDateTime) => {
+    let selectedDate = new Date(selectedDateTime);
+    let formattedDate = moment(new Date(selectedDate)).format('LL') || date;
+    setShow(Platform.OS === 'ios');
+    setDate(formattedDate);
+  };
+
+  const showMode = currentMode => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  const showDatepicker = () => {
+    showMode('date');
   };
 
   return (
@@ -41,10 +79,21 @@ const AddEditModal = props => {
       {/* <TextBox func={onChangeText} value={text} /> */}
       {TextBox(onChangeAmt, amount, 'Amount')}
       {TextBox(onChangeDesc, desc, 'Description')}
-      {TextBox(onChangeDate, date, 'Date')}
-      <TouchableOpacity style={{flex: 0.2, alignItems: 'center'}}>
+      {DateBox(date, 'Date')}
+      <TouchableOpacity
+        style={{flex: 0.2, alignItems: 'center', justifyContent: 'center'}}>
         <Text style={{color: colors.accent}}>Save</Text>
       </TouchableOpacity>
+      {show && (
+        <DateTimePicker
+          testID="dateTimePicker"
+          value={dateTime}
+          mode={mode}
+          is24Hour={true}
+          display="default"
+          onChange={onChange}
+        />
+      )}
     </View>
   );
 };
@@ -62,8 +111,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 10,
     borderColor: '#E5E5E5',
-    margin: 10,
+    margin: 15,
     paddingLeft: 15,
+    color: colors.lightblack,
   },
 });
 

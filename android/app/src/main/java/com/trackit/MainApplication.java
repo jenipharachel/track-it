3c1532043d274d32c872d94793761c88c85dd625
Jenipha Rachel <jenipha.rachel@bigbinary.com>
2021-07-19 17:52:43 +0530

    persist data with async storage and redux-saga

--- android/app/src/main/java/com/trackit/MainApplication.java
+++ android/app/src/main/java/com/trackit/MainApplication.java
@@ -9,8 +9,8 @@ import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.facebook.react.bridge.JSIModulePackage;
-import com.reactnativemmkv.MmkvModulePackage;
+// import com.facebook.react.bridge.JSIModulePackage;
+// import com.reactnativemmkv.MmkvModulePackage;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
@@ -37,10 +37,10 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
 
-         @Override
-        protected JSIModulePackage getJSIModulePackage() {
-          return new MmkvModulePackage();
-        }
+        //  @Override
+        // protected JSIModulePackage getJSIModulePackage() {
+        //   return new MmkvModulePackage();
+        // }
       };
 
   @Override

cdb56055dbd55f0b6968ebaa4bbb16733096dd5d
Jenipha Rachel <jenipha.rachel@bigbinary.com>
2021-07-20 11:14:04 +0530

    removed middleware since it's not required for this task

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,16 @@
-import {applyMiddleware, createStore} from 'redux';
+import {createStore} from 'redux';
 import {persistStore, persistReducer} from 'redux-persist';
 // import AsyncStorage from '@react-native-async-storage/async-storage';
-import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import {storage} from '../utils/storage';
-// import {rootSaga} from './sagas';
-
-const middlewareToApply = [];
-const sagaMiddleware = createSagaMiddleware();
-
-middlewareToApply.push(sagaMiddleware);
 
 const persistConfig = {
   key: 'root',
   storage: storage,
 };
 
-const middlewares = applyMiddleware(...middlewareToApply);
 const persistedReducer = persistReducer(persistConfig, reducers);
-const Store = createStore(persistedReducer, middlewares);
-// sagaMiddleware.run(rootSaga);
+const Store = createStore(persistedReducer);
 export const Persistor = persistStore(Store);
 
 export default Store;
